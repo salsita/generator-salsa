950c49ee0e0a7da824c6c175c939b6320e778abf
oilart <oilart@live.com>
2014-07-31 11:38:48 +0200

    angular 1.2.x, updated app scripts, bower.json

--- app/templates/app/scripts/app.js
+++ app/templates/app/scripts/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
-angular.module('<%= _.camelize(appname)%>App', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('<%= _.camelize(appname)%>App', [
+  'ngRoute',
+  'ui.router'
+])
+.config(function ($stateProvider) {
+  $stateProvider
+    .state('default', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      url: '/'
+    })
+    .state('otherwise', {
+      url: '/'
+    });
+});
