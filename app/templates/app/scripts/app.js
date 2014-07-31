baeb1c86d24045fb511d7d6b4433425815d9d752
oilart <oilart@live.com>
2014-07-31 15:01:14 +0200

    updated bower js, app routes, html5 mode

--- app/templates/app/scripts/app.js
+++ app/templates/app/scripts/app.js
@@ -4,7 +4,9 @@ angular.module('<%= _.camelize(appname)%>App', [
   'ngRoute',
   'ui.router'
 ])
-.config(function ($stateProvider) {
+.config(function ($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
   $stateProvider
     .state('default', {
       templateUrl: 'views/main.html',
